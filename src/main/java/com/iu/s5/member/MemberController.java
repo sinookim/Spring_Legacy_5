a8bcd94b8e3ba52d3b1158fdb7acae3182424a4c
playcrabs <45160640+playcrabs@users.noreply.github.com>
2020-04-23 09:14:54 +0900

    fix

--- src/main/java/com/iu/s5/member/MemberController.java
+++ src/main/java/com/iu/s5/member/MemberController.java
@@ -25,138 +25,134 @@ public class MemberController {
 
 	@Autowired
 	private MemberService memberService;
-	
-	@RequestMapping(value="memberList", method = RequestMethod.GET)
-	public ModelAndView memberList(Pager memberPager, ModelAndView mv)throws Exception{
+
+	@RequestMapping(value = "memberList", method = RequestMethod.GET)
+	public ModelAndView memberList(Pager memberPager, ModelAndView mv) throws Exception {
 		List<MemberVO> ar = memberService.memberList(memberPager);
-		
+
 		mv.addObject("list", ar);
 		mv.addObject("pager", memberPager);
 		mv.setViewName("member/memberList");
-		
+
 		return mv;
 	}
-	
+
 	@RequestMapping(value = "memberLogout")
-	public String memberLogout(HttpSession session)throws Exception{
+	public String memberLogout(HttpSession session) throws Exception {
 		session.invalidate();
 		return "redirect:../";
 	}
 
-	@RequestMapping(value= "memberJoin")
+	@RequestMapping(value = "memberJoin")
 	public void memberJoin() {
-		
+
 	}
-	
-	@RequestMapping(value= "memberJoin", method = RequestMethod.POST)
-	public ModelAndView memberJoin(MemberVO memberVO, MultipartFile avatar, ModelAndView mv, HttpSession session) throws Exception {
-		
-		
+
+	@RequestMapping(value = "memberJoin", method = RequestMethod.POST)
+	public ModelAndView memberJoin(MemberVO memberVO, MultipartFile avatar, ModelAndView mv, HttpSession session)
+			throws Exception {
+
 		int result = memberService.memberJoin(memberVO, avatar, session);
-		String msg ="Member Join Fail";
-		if(result>0) {
+		String msg = "Member Join Fail";
+		if (result > 0) {
 			msg = "Member Join Success";
 		}
-		
+
 		mv.addObject("result", msg);
 		mv.addObject("path", "../");
 		mv.setViewName("common/result");
-		
+
 		return mv;
 	}
-	
-	@RequestMapping(value= "memberLogin")
+
+	@RequestMapping(value = "memberLogin")
 	public void memberLogin(@CookieValue(value = "cId", required = false) String cId, Model model) {
 		System.out.println(cId);
-		//model.addAttribute("cId", cId);
+		// model.addAttribute("cId", cId);
 	}
-	
-	@RequestMapping(value= "memberLogin", method = RequestMethod.POST)
-	public ModelAndView memberLogin(ModelAndView mv,String remember, MemberVO memberVO, HttpSession session, HttpServletResponse response) throws Exception {
+
+	@RequestMapping(value = "memberLogin", method = RequestMethod.POST)
+	public ModelAndView memberLogin(ModelAndView mv, String remember, MemberVO memberVO, HttpSession session,
+			HttpServletResponse response) throws Exception {
 		Cookie cookie = new Cookie("cId", "");
-		
-		if(remember != null) {
+
+		if (remember != null) {
 			/* cookie = new Cookie("cId", memberVO.getId()); */
 			cookie.setValue(memberVO.getId());
 		}
-		
+
 		/* cookie.setMaxAge(0); */
 		/* cookie.setValue(newValue); */
 		response.addCookie(cookie);
-		
-		 memberVO = memberService.memberLogin(memberVO);
-		
-		 if(memberVO != null) {
-			 session.setAttribute("member", memberVO);
-			 mv.setViewName("redirect:../");
-		 }else {
-			 mv.addObject("result", "Login Fail");
-			 mv.addObject("path", "./memberJoin");
-			 mv.setViewName("common/result");
-		 }
-		 
-		//로그인 성공이면 index
-		//로그인 실패 하면 로그인 실패 alert login form 이동		 
-				 
-				 
+
+		memberVO = memberService.memberLogin(memberVO);
+
+		if (memberVO != null) {
+			session.setAttribute("member", memberVO);
+			mv.setViewName("redirect:../");
+		} else {
+			mv.addObject("result", "Login Fail");
+			mv.addObject("path", "./memberJoin");
+			mv.setViewName("common/result");
+		}
+
+		// 로그인 성공이면 index
+		// 로그인 실패 하면 로그인 실패 alert login form 이동
+
 		return mv;
-	}
-	
-	@RequestMapping(value= "memberPage")
-	public void memberPage(HttpSession session, Model model) throws Exception {
-		MemberVO memberVO = (MemberVO)session.getAttribute("member");
-		MemberFileVO memberFileVO = memberService.fileSelect(memberVO.getId());
-		model.addAttribute("file", memberFileVO);
 	}
 
-	
-	@RequestMapping(value= "memberUpdate")
-	public void memberUpdate() {
-		
+	@RequestMapping(value = "memberPage")
+	public void memberPage() throws Exception {
 	}
-	
-	@RequestMapping(value= "memberUpdate", method = RequestMethod.POST)
+
+	@RequestMapping(value = "memberUpdate")
+	public void memberUpdate() {
+
+	}
+
+	@RequestMapping(value = "memberUpdate", method = RequestMethod.POST)
 	public ModelAndView memberUpdate(ModelAndView mv, MemberVO memberVO, HttpSession session) throws Exception {
-		String id = ((MemberVO)session.getAttribute("member")).getId();
+		String id = ((MemberVO) session.getAttribute("member")).getId();
 		memberVO.setId(id);
-		
+
 		int result = memberService.memberUpdate(memberVO);
-		
-		if(result>0) {
+
+		if (result > 0) {
 			session.setAttribute("member", memberVO);
 			mv.setViewName("redirect:./memberPage");
-		}else {
-			 mv.addObject("result", "Update Fail");
-			 mv.addObject("path", "./memberPage");
-			 mv.setViewName("common/result");
+		} else {
+			mv.addObject("result", "Update Fail");
+			mv.addObject("path", "./memberPage");
+			mv.setViewName("common/result");
 		}
-		
+
 		return mv;
 	}
-	
-	@RequestMapping(value= "memberDelete")
+
+	@RequestMapping(value = "memberDelete")
 	public ModelAndView memberDelete(ModelAndView mv, HttpSession session) throws Exception {
-		MemberVO memberVO = (MemberVO)session.getAttribute("member");
+		MemberVO memberVO = (MemberVO) session.getAttribute("member");
 		int result = memberService.memberDelete(memberVO);
-		if(result>0) {
+		if (result > 0) {
 			session.invalidate();
 			mv.addObject("result", "Delete Success");
 			mv.addObject("path", "../");
 			mv.setViewName("common/result");
-		}else {
+		} else {
 			mv.addObject("result", "Delete Fail");
 			mv.addObject("path", "../");
 			mv.setViewName("common/result");
 		}
-		
+
 		return mv;
 	}
-	
+
 	@GetMapping("fileDelete")
-	public String fileDelete(HttpSession session)throws Exception{
-		MemberVO memberVO = (MemberVO)session.getAttribute("member");
+	public String fileDelete(HttpSession session) throws Exception {
+		MemberVO memberVO = (MemberVO) session.getAttribute("member");
 		memberService.fileDelete(memberVO.getId(), session);
 		return "redirect:./memberPage";
 	}
-	
+
 }
